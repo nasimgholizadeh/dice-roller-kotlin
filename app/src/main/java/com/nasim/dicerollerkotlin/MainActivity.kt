f6828671b4a91bfa9b6883a2c742a064ddc1c838
nasimgholizadeh <nasim.2000ngh@gmail.com>
2021-03-27 14:15:48 +0430

    initial commit

---
+++ app/src/main/java/com/nasim/dicerollerkotlin/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.nasim.dicerollerkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
+    private lateinit var btnRoll: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_img)
+        btnRoll = findViewById(R.id.btn_roll)
+        btnRoll.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+}
